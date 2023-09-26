ea965452b0295c914100a4ad973bb3feb87307f7
ryuwa <ryuusuzuki243@gmail.com>
2023-09-26 15:32:25 +0900

    404ページの作成

---
+++ 404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ページが見つかりませんでした</title>
+</head>
+<body>
+    Not Found!!
+</body>
+</html>
